e4b6cf3055835826bd4fcd7585bec6e366e38064
Ivan Montesinos <ivamonmor2@alu.edu.gva.es>
2025-05-21 18:15:33 +0200

    initial commit

---
+++ app/src/main/java/es/ieslavereda/miravereda/MainActivity.java
@@ -0,0 +1,43 @@
+package es.ieslavereda.miravereda;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView ivLogo;
+    private ImageView ivPreference;
+    private TextInputEditText username;
+    private TextInputEditText password;
+    private TextView tvContrasenya;
+    private Button btIniciarSesion;
+    private TextView tvCrearCuenta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ivLogo=findViewById(R.id.ivLogo);
+        ivPreference=findViewById(R.id.ivPreference);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        tvContrasenya=findViewById(R.id.tvContrasenya);
+        btIniciarSesion=findViewById(R.id.btIniciarSesion);
+        tvCrearCuenta=findViewById(R.id.tvCrearCuenta);
+    }
+}
---
+++ app/src/main/java/es/ieslavereda/miravereda/Model/Usuario.java
@@ -0,0 +1,56 @@
+package es.ieslavereda.miravereda.Model;
+
+import java.sql.Timestamp;
+
+public class Usuario {
+    private int id;
+    private String contrasenya;
+    private String nombre;
+    private String apellidos;
+    private String domicilio;
+    private String cod_postal;
+    private String email;
+    private long fecha_nac;
+    private String num_tarjeta;
+    private Timestamp changedTs;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public String getCod_postal() {
+        return cod_postal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public String getNum_tarjeta() {
+        return num_tarjeta;
+    }
+
+    public Timestamp getChangedTs() {
+        return changedTs;
+    }
+}
