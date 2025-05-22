2c964e867d6985d473e8d63c9c28bd4fd7da863d
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-05-22 19:44:17 +0200

    añadido mas triggers y prods

--- incompleto_FUNCIONES_MIRAVEREDA_DUMP.sql
+++ incompleto_FUNCIONES_MIRAVEREDA_DUMP.sql
@@ -3,8 +3,56 @@
 
 use miravereda2425;
 
+
+-- #### TABLA CLIENTE ####
+-- PROCEDIMIENTO y TRIGGERS para actualizar el changedTS de CLIENTE
+delimiter $$
+drop procedure if exists actualizar_changedTS_cliente$$
+create procedure actualizar_changedTS_cliente(in cliente_id int)
+begin
+	update cliente set changedTS = now() where id = cliente_id;
+end
+$$
+delimiter ;
+
+-- TRIGGER INSERT
+delimiter $$
+drop trigger if exists trigger_cliente_insert$$
+create trigger trigger_cliente_insert
+after insert on cliente for each row
+begin
+	call actualizar_changedTS_cliente(new.id);
+end
+$$
+delimiter ;
+
+-- TRIGGER UPDATE
+delimiter $$
+drop trigger if exists trigger_cliente_update$$
+create trigger trigger_cliente_update
+after update on cliente for each row
+begin
+	call actualizar_changedTS_cliente;
+end
+$$
+delimiter ;
+
+-- TRIGGER al hacer INSERT en la tabla CLIENTE
+-- se crea un CARRITO asignado a este
+delimiter $$
+drop trigger if exists trigger_cliente_carrito$$
+create trigger trigger_cliente_carrito
+after insert on cliente for each row
+begin
+	insert into carrito(cliente_id, total, changedTS)
+	values (new.id, 0, now());
+end
+$$
+delimiter ;
+
+
 -- #### TABLA VALORACION ####
--- TRIGGER al hacer INSERT en VALORACION para que la nota no sea mayor a 10 ni menor que 0
+-- TRIGGER al hacer INSERT en VALORACION para que la nota no sea mayor a 10 ni menor que 0, también el changedTS es now()
 delimiter $$
 drop trigger if exists trigger_valoracion_insert$$
 create trigger trigger_valoracion_insert
@@ -13,6 +61,7 @@ begin
 	if new.valor >10 then set new.valor = 10;
 	elseif new.valor < 0 then set new.valor = 0;
 	end if;
+	set new.changedTS = now();
 end
 delimiter ;
 
@@ -45,6 +94,27 @@ create trigger trigger_actualizar_nota
 after insert on valoracion for each row
 begin
 	call actualizar_nota(new.contenido_id);
+end
+$$
+delimiter ;
+
+-- #### TABLA CONTENIDO ####
+-- PROCEDIMIENTO actualizar_changedTS_contenido
+delimiter $$
+drop procedure if exists actualizar_changedTS_contenido$$
+create procedure actualizar_changedTS_contenido(in contenido_id int)
+begin
+	update contenido set changedTS = now() where id = contenido_id;
+end
+$$
+delimiter ;
+-- TRIGGERS para changedTS
+delimiter $$
+drop trigger if exists trigger_contenido_insert$$
+create trigger trigger_contenido_insert
+after insert on contenido for each row
+begin
+	call actualizar_changedTS_contenido(new.id)
 end
 $$
 delimiter ;
