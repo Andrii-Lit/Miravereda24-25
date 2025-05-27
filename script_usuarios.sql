e329dcb493106ab77fd6de90ad0d02be10dd36d7
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-05-27 10:41:36 +0200

    script de usuarios

---
+++ script_usuarios.sql
@@ -0,0 +1,26 @@
+insert into cliente 
+(contrasenya, nombre, apellidos, domicilio, cod_postal, email, fecha_nac, num_tarjeta)
+values
+-- Juan Cuesta, presidente de la comunidad
+('juan123', 'Juan', 'Cuesta', 'Desengaño 21, 2ºA', '28004', 'juan.cuesta@miravereda.es', '1965-03-14', '1234567812345678'),
+
+-- Emilio Delgado
+('emilio456', 'Emilio', 'Delgado', 'Desengaño 21, portería', '28004', 'emilio.delgado@miravereda.es', '1978-07-22', '2345678923456789'),
+
+-- Lucía Álvarez
+('lucia789', 'Lucía', 'Álvarez', 'Desengaño 21, 3ºB', '28004', 'lucia.alvarez@miravereda.es', '1980-11-05', '3456789034567890'),
+
+-- Mauri Hidalgo
+('mauri101', 'Mauri', 'Hidalgo', 'Desengaño 21, 1ºC', '28004', 'mauri.hidalgo@miravereda.es', '1972-02-10', '4567890145678901'),
+
+-- Belén López
+('belen202', 'Belén', 'López', 'Desengaño 21, 3ºA', '28004', 'belen.lopez@miravereda.es', '1979-08-30', '5678901256789012'),
+
+-- Marisa Benito
+('marisa303', 'Marisa', 'Benito', 'Desengaño 21, 1ºA', '28004', 'marisa.benito@miravereda.es', '1942-06-12', '6789012367890123'),
+
+-- Vicenta Benito
+('vicenta404', 'Vicenta', 'Benito', 'Desengaño 21, 1ºA', '28004', 'vicenta.benito@miravereda.es', '1945-09-18', '7890123478901234'),
+
+-- Concha
+('concha505', 'Concha', 'Jiménez', 'Desengaño 21, 2ºB', '28004', 'concha.jimenez@miravereda.es', '1938-01-27', '8901234589012345');
