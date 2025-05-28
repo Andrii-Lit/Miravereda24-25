68dddaf19f6f960b639604dbfad44f5fc47b3dc0
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-05-28 14:18:46 +0200

    SCRIPT DDL CON INSERTS, TRIGGERS Y PROD AÑADIDOS

--- DDL_miravereda2425.sql
+++ DDL_miravereda2425.sql
@@ -1,12 +1,20 @@
--- #### SCRIPT PARA CREAR LA BASE DE DATOS MIRAVEREDA
--- by Cristóbal
--- Se han dejado campos sin NOT NULL para hacer pruebas
--- BASE DE DATOS
+#--------------------------------------------------------
+#-- SCRIPT PARA CREAR LA BASE DE DATOS MIRAVEREDA
+#--------------------------------------------------------
+#-- by Cristóbal
+#-- Se han dejado campos sin NOT NULL para hacer pruebas
+#--------------------------------------------------------
+
+#-----------------
+#-- BASE DE DATOS
+#-----------------
 drop database if exists miravereda2425;
 create database miravereda2425;
 use miravereda2425;
 
--- tabla CLIENTE
+#------------
+#-- CLIENTE
+#------------
 create table cliente (
     id int auto_increment primary key,
     contrasenya varchar(255),
@@ -14,15 +22,19 @@ create table cliente (
     apellidos varchar(100),
     domicilio varchar(255),
     cod_postal varchar(50),
-    email varchar(100) unique,
+    email varchar(100) unique,----------
     fecha_nac date,
     num_tarjeta varchar(100),
     changedTS timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
--- #### CONTENIDO AUDIOVISUAL ####
+#-----------------------------------
+#-- TABLAS DE CONTENIDO AUDIOVISUAL 
+#-----------------------------------
 
--- tabla CONTENIDO
+#--------------
+#-- CONTENIDO
+#--------------
 create table contenido (
  id int primary key,
  titulo varchar(255),
@@ -35,19 +47,21 @@ create table contenido (
  puntuacion_media decimal(3,1),
  poster_path varchar(255),
  changedTS timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
-
 );
 
--- tabla TARIFA
+#-----------
+#-- TARIFA
+#-----------
 create table tarifa(
-    id int auto_increment primary key,
+    id int primary key,
     tipo varchar(50),
     porcentaje decimal(5,4),
     changedTS timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 
 );
-
--- tabla VALORACION
+#---------------
+#-- VALORACION
+#---------------
 create table valoracion (
     cliente_id int,
     contenido_id int,
@@ -59,7 +73,9 @@ create table valoracion (
 
 );
 
--- tabla PELICULA
+#-------------
+#-- PELICULA
+#-------------
 create table pelicula(
     contenido_id int primary key,
     tarifa_id int,
@@ -70,7 +86,9 @@ create table pelicula(
     foreign key (tarifa_id) references tarifa(id)
 );
 
--- tabla SERIE
+#----------
+#-- SERIE
+#----------
 create table serie(
     contenido_id int primary key,
     tarifa_id int,
@@ -81,7 +99,9 @@ create table serie(
     foreign key (tarifa_id) references tarifa(id)
 );
 
--- tabla CORTO
+#----------
+#-- CORTO
+#----------
 create table corto(
     contenido_id int primary key,
     tarifa_id int,
@@ -92,7 +112,9 @@ create table corto(
     foreign key (tarifa_id) references tarifa(id)
 );
 
--- tabla TEMPORADA
+#--------------
+#-- TEMPORADA
+#--------------
 create table temporada(
     id int auto_increment primary key,
     serie_id int,
@@ -102,18 +124,26 @@ create table temporada(
     unique (serie_id, numero)
 );
 
--- tabla CAPITULO
+#-------------
+#-- CAPITULO
+#-------------
 create table capitulo(
     id int auto_increment primary key,
     temporada_id int,
     titulo varchar(255),
+    precio_base decimal(10,2),
     precio decimal(10,2),
     changedTS timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     foreign key (temporada_id) references temporada(id)
 );
 
--- #### COMERCIO ####
--- tabla CARRITO
+#-----------------------
+#-- TABLAS DE COMERCIO
+#-----------------------
+
+#------------
+#-- CARRITO
+#------------
 create table carrito(
     id int auto_increment primary key,
     cliente_id int,
@@ -123,7 +153,9 @@ create table carrito(
     foreign key (cliente_id) references cliente (id) on delete cascade
 );
 
--- tabla LIN_FAC
+#------------
+#-- LIN_FAC
+#------------
 create table lin_fac(
     carrito_id int,
     contenido_id int,
@@ -135,7 +167,9 @@ create table lin_fac(
 
 );
 
--- tabla FACTURA
+#------------
+#-- FACTURA
+#------------
 create table factura(
     id int primary key auto_increment,
     cliente_id int,
@@ -146,3 +180,92 @@ create table factura(
     changedTS timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     foreign key (cliente_id) references cliente (id) on delete cascade
 );
+
+#----------------------
+#-- INSERTAMOS TARIFAS
+#----------------------
+insert into tarifa(id, tipo, porcentaje)
+values (1,'pelicula', 0.20), (2,'serie', 0.10), (3,'corto', 0.05);
+
+#-------------------------------
+#-- INSERTAMOS ALGUNOS CLIENTES
+#-------------------------------
+insert into cliente 
+(contrasenya, nombre, apellidos, domicilio, cod_postal, email, fecha_nac, num_tarjeta)
+values
+-- Usuario por defecto para pruebas, antes de meter a media finca de Aquí no hay quien viva
+('1234', 'abc', 'def', 'direccion1', '46160', 'abc@gmail.com', '2000-01-01', '1234567812345679'),
+
+-- Juan Cuesta, presidente de la comunidad
+('juan123', 'Juan', 'Cuesta', 'Desengaño 21, 2ºA', '28004', 'juan.cuesta@miravereda.es', '1965-03-14', '1234567812345678'),
+
+-- Emilio Delgado
+('emilio456', 'Emilio', 'Delgado', 'Desengaño 21, portería', '28004', 'emilio.delgado@miravereda.es', '1978-07-22', '2345678923456789'),
+
+-- Lucía Álvarez
+('lucia789', 'Lucía', 'Álvarez', 'Desengaño 21, 3ºB', '28004', 'lucia.alvarez@miravereda.es', '1980-11-05', '3456789034567890'),
+
+-- Mauri Hidalgo
+('mauri101', 'Mauri', 'Hidalgo', 'Desengaño 21, 1ºC', '28004', 'mauri.hidalgo@miravereda.es', '1972-02-10', '4567890145678901'),
+
+-- Belén López
+('belen202', 'Belén', 'López', 'Desengaño 21, 3ºA', '28004', 'belen.lopez@miravereda.es', '1979-08-30', '5678901256789012'),
+
+-- Marisa Benito
+('marisa303', 'Marisa', 'Benito', 'Desengaño 21, 1ºA', '28004', 'marisa.benito@miravereda.es', '1942-06-12', '6789012367890123'),
+
+-- Vicenta Benito
+('vicenta404', 'Vicenta', 'Benito', 'Desengaño 21, 1ºA', '28004', 'vicenta.benito@miravereda.es', '1945-09-18', '7890123478901234'),
+
+-- Concha
+('concha505', 'Concha', 'Jiménez', 'Desengaño 21, 2ºB', '28004', 'concha.jimenez@miravereda.es', '1938-01-27', '8901234589012345');
+
+#-----------------------------------------------
+#-- INSERTAMOS ALGUNOS CONTENIDOS AUDIOVISUALES
+#-----------------------------------------------
+
+#-----------------------------------------------
+#-- PELICULA
+#-----------------------------------------------
+-- Contenido base
+insert into contenido (id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
+values (1, 'Inception', 'Un ladrón que roba secretos corporativos a través del uso de la tecnología de sueños compartidos.', 'Ciencia Ficción', 'Christopher Nolan', 148, 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', '2010-07-16', 8.8, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflxt.tmsimg.com%2Fassets%2Fp7825626_p_v8_af.jpg&f=1&nofb=1&ipt=43b10321849c53c4bf641c883af9e87240c3a714164beb0b0845580bc31549fc');
+
+-- Película asociada
+insert into pelicula (contenido_id, tarifa_id, disponible_hasta, precio_base)
+values (1, 1, '2025-12-31', 12.99);
+
+#-----------------------------------------------
+#-- SERIE
+#-----------------------------------------------
+-- Contenido base
+insert into contenido (id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
+values (2, 'Chernobyl', 'Miniserie sobre el desastre nuclear de Chernóbil en 1986.', 'Drama histórico', 'Johan Renck', 330, 'Jared Harris, Stellan Skarsgård, Emily Watson', '2019-05-06', 9.4, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.themoviedb.org%2Ft%2Fp%2Foriginal%2F4cPTQbF2GL59j2YGeUaBg3yvzJw.jpg&f=1&nofb=1&ipt=6c3ac1761bf53d9f25cc472e1f9aa7449f7d78ddfa9bd9dde942c3afde839238');
+
+-- Serie asociada
+insert into serie (contenido_id, tarifa_id, disponibilidad, precio_base)
+values (2, 2, true, 19.99);
+
+-- Temporada
+insert into temporada (serie_id, numero)
+values (2, 1);
+
+-- Capítulos (ejemplo con 5)
+insert into capitulo (temporada_id, titulo, precio_base, precio_base)
+values
+(1, '1.1 - 1:23:45', 3.00, 3.30),
+(1, '1.2 - Please Remain Calm', 3.00, 3.30),
+(1, '1.3 - Open Wide, O Earth', 3.00, 3.30),
+(1, '1.4 - The Happiness of All Mankind', 3.00, 3.30),
+(1, '1.5 - Vichnaya Pamyat', 3.00, 3.30);
+
+#-----------------------------------------------
+#-- CORTO
+#-----------------------------------------------
+-- Contenido base
+insert into contenido (id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
+values (3, 'Piper', 'Un pequeño pájaro de la orilla del mar aprende a encontrar su alimento en las olas.', 'Animación', 'Alan Barillaro', 6, '—', '2016-06-17', 8.4, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflxt.tmsimg.com%2Fassets%2Fp12993868_p_v10_aa.jpg&f=1&nofb=1&ipt=1044bca340e9034079917c1f9ef5279dfa49ecd5bad317c2335c731fa69f37fd');
+
+-- Corto asociado
+insert into corto (contenido_id, tarifa_id, precio_base)
+values (3, 3, 1.99);
--- incompleto_TRIGGERS_PROD_MIRAVEREDA_DUMP.sql
+++ incompleto_TRIGGERS_PROD_MIRAVEREDA_DUMP.sql
@@ -1,102 +1,39 @@
--- DUMP PROCEDIMIENTOS Y TRIGGERS SQL
--- by Cristóbal
+#---------------------------------------
+#-- DUMP PROCEDIMIENTOS Y TRIGGERS SQL
+#-- by Cristóbal
+#---------------------------------------
 
 use miravereda2425;
 
--- #### FUNCIONES ####
--- PROCEDIMIENTO para iniciar sesión que devuelva un booleano
+#--------------------------------------------
+#-- FUNCIONALIDADES DE LA APLICACIÓN CLIENTE
+#--------------------------------------------
+
+#-----------------------------------------------------------------------
+#-- PROCEDIMIENTO para el botón INICIAR SESIÓN que devuelva un booleano
+#-----------------------------------------------------------------------
 delimiter $$
 drop procedure if exists iniciar_sesion$$
 create procedure iniciar_sesion(out resultado boolean, in p_email varchar(100), in p_contrasenya varchar(255))
 begin
 	declare contra_correcta varchar(255);
+
+	-- Sacamos la contraseña asociada al email por parámetro en la bbdd
 	select contrasenya into contra_correcta from cliente where email = p_email limit 1;
 	
+	-- y si coincide con la que le pasamos por parámetro devuelve true
 	if contra_correcta is null 
 		then set resultado = false;
 	elseif p_contrasenya = contra_correcta 
 		then set resultado = true;
 	else set resultado = false;
 	end if;
-end
-$$
+end$$
 delimiter ;
 
--- #### TABLA CLIENTE ####
--- TRIGGER al hacer INSERT en la tabla CLIENTE
--- se crea un CARRITO asignado a este
-delimiter $$
-drop trigger if exists trigger_cliente_carrito$$
-create trigger trigger_cliente_carrito
-after insert on cliente for each row
-begin
-	insert into carrito(cliente_id)values (new.id);
-end
-$$
-delimiter ;
-
--- PROCEDIMIENTO para obtener CLIENTE por email
-delimiter $$ 
-drop procedure if exists get_cliente_por_email$$
-
-create procedure get_cliente_por_email(in p_email VARCHAR(100))
-begin
-    select * from cliente where email = p_email limit 1;
-END
-$$
-DELIMITER ;
-
-
-
--- #### TABLA VALORACION ####
--- TRIGGER al hacer INSERT en VALORACION para que la nota no sea mayor a 10 ni menor que 0
-delimiter $$
-drop trigger if exists trigger_valoracion_insert$$
-create trigger trigger_valoracion_insert
-before insert on valoracion for each row
-begin
-	if new.valor >10 then set new.valor = 10;
-	elseif new.valor < 0 then set new.valor = 0;
-	end if;
-end
-$$
-delimiter ;
-
--- PROCEDIMIENTO para actualizar la PUNTUACION_MEDIA de CONTENIDO 
--- (Se llamará desde TRIGGER_ACTUALIZAR_NOTA)
-delimiter $$
-drop procedure if exists actualizar_nota$$
-create procedure actualizar_nota(IN p_cliente_id int)
-begin
-	declare puntos_totales decimal(10,2);
-	declare cantidad int;
-	declare media decimal(10,2);
-	
-	select sum(valor) into puntos_totales from valoracion where contenido_id = p_cliente_id;
-	select count(*) into cantidad from valoracion where contenido_id = p_cliente_id;
-	
-	if cantidad > 0 then set media = puntos_totales/cantidad;
-	else set media = 0;
-	end if;
-	
-	update contenido set puntuacion_media = media;
-end
-$$
-delimiter ;
-
--- TRIGGER que llama a ACTUALIZAR_NOTA al hacer INSERT en VALORACION
-delimiter $$
-drop trigger if exists trigger_actualizar_nota$$
-create trigger trigger_actualizar_nota
-after insert on valoracion for each row
-begin
-	call actualizar_nota(new.contenido_id);
-end
-$$
-delimiter ;
-
--- #### TABLA FACTURA ####
--- PROCEDIMIENTO al que llamará el botón COMPRAR en carrito
+#------------------------------------------------------------
+#-- PROCEDIMIENTO al que llamará el botón COMPRAR en carrito
+#------------------------------------------------------------
 delimiter $$
 drop procedure if exists comprar$$
 create procedure comprar(IN p_cliente_id int)
@@ -107,23 +44,147 @@ begin
 
 	set total_con_iva = carrito_total + (carrito_total * iva_rate);
 
-	-- inserta en factura el id, total sin y con
+	-- Hace INSERT en FACTURA el cliente_id, total sin y con el IVA
 	insert into factura(cliente_id, total, total_con_iva)
 	values (p_cliente_id, carrito_total, total_con_iva);
 
-	-- desactiva el carrito
+	-- Desactiva el CARRITO
 	update carrito set activo = false where cliente_id = p_cliente_id and activo = true;
 
-    -- al final crea un nuevo carrito
+    -- Al final crea un nuevo CARRITO
 	insert into carrito(cliente_id) values (p_cliente_id);
-    
-end
-$$
+end$$
 delimiter ;
 
--- #### TABLA CARRITO ####
--- PROCEDIMIENTO que actualiza el precio del carrito activo según las líneas de factura
--- le pasamos el cliente_id para identificar el carrito activo
+#---------------------------------------------------------------------
+#-- PROCEDIMIENTO para el botón de VOTAR para insertar una VALORACION
+#---------------------------------------------------------------------
+delimiter $$
+drop procedure if exists votar$$
+create procedure votar(in p_cliente_id, p_contenido_id, p_valor)
+begin
+	insert into valoracion (cliente_id, contenido_id, valor)
+	values(p_cliente_id, p_contenido_id, p_valor);
+end$$
+delimiter ;
+
+#--------------------------------------------------------------------------------------------------
+#-- PROCEDIMIENTO al que llamará el botón de AÑADIR AL CARRITO en la vista detallada del contenido
+#-- le pasamos el cliente_id porque buscará luego el carrito activo del cliente
+#--------------------------------------------------------------------------------------------------
+delimiter $$
+drop procedure if exists anyadir_al_carrito$$
+create procedure anyadir_al_carrito(in p_cliente_id int, in p_contenido_id int)
+begin
+	declare carrito_activo_id int;
+	declare contenido_precio decimal(10,2);
+	
+	-- Recogemos el id del carrito activo del cliente
+	select id into carrito_activo_id from carrito
+	where cliente_id = p_cliente_id and activo = true;
+	
+	-- Recogemos el precio según el tipo de contenido
+	if p_contenido_id in (select contenido_id from pelicula)
+		then set contenido_precio = (select precio from pelicula where contenido_id = p_contenido_id);
+	 elseif p_contenido_id in (select contenido_id from serie)
+	 	then set contenido_precio = (select precio from serie where contenido_id = p_contenido_id);
+	 elseif p_contenido_id in (select contenido_id from corto)
+	 	then set contenido_precio = (select precio from corto where contenido_id = p_contenido_id);
+	else set contenido_precio = 0.00;
+	end if; 	
+	insert into lin_fac(carrito_id, contenido_id, precio) 
+	values (carrito_activo_id, p_contenido_id, contenido_precio);
+
+	-- Actualizamos el precio del carrito
+	call actualizar_precio_carrito(p_cliente_id);
+end$$
+delimiter ;
+
+#------------------------------------------
+#-- PROCEDIMIENTOS Y TRIGGERS para la BBDD
+#------------------------------------------
+
+#----------------------------------------------------------------------------------
+#-- TABLA CLIENTE
+#----------------------------------------------------------------------------------
+#-- TRIGGER al hacer INSERT en la tabla CLIENTE se crea un CARRITO asignado a este
+#----------------------------------------------------------------------------------
+delimiter $$
+drop trigger if exists trigger_cliente_carrito$$
+create trigger trigger_cliente_carrito
+after insert on cliente for each row
+begin
+	insert into carrito(cliente_id) values (new.id);
+end$
+delimiter ;
+#-------------------------------------------------
+#-- PROCEDIMIENTO para obtener CLIENTE por email
+#-- Hecho por Iván
+#-------------------------------------------------
+delimiter $$ 
+drop procedure if exists get_cliente_por_email$$
+create procedure get_cliente_por_email(in p_email VARCHAR(100))
+begin
+    select * from cliente where email = p_email limit 1;
+END$$
+DELIMITER ;
+
+#-------------------------------------------------------------------------------------------
+#-- TABLA VALORACION 
+#-------------------------------------------------------------------------------------------
+#-- TRIGGER al hacer INSERT en VALORACION para que la nota no sea mayor a 10 ni menor que 0
+#-------------------------------------------------------------------------------------------
+delimiter $$
+drop trigger if exists trigger_valoracion_insert$$
+
+create trigger trigger_valoracion_insert
+before insert on valoracion for each row
+begin
+	if new.valor >10 then set new.valor = 10;
+	elseif new.valor < 0 then set new.valor = 0;
+	end if;
+end$$
+delimiter ;
+#-------------------------------------------------------------------
+#-- PROCEDIMIENTO para actualizar la PUNTUACION_MEDIA de CONTENIDO 
+#-- Se llamará desde TRIGGER_ACTUALIZAR_NOTA
+#-------------------------------------------------------------------
+delimiter $$
+drop procedure if exists actualizar_nota$$
+create procedure actualizar_nota(IN p_contenido_id int)
+begin
+	declare puntos_totales decimal(10,2);
+	declare cantidad int;
+	declare media decimal(10,2);
+
+	select sum(valor) into puntos_totales from valoracion where contenido_id = p_contenido_id;
+	select count(*) into cantidad from valoracion where contenido_id = p_contenido_id;
+	
+	if cantidad > 0 then set media = puntos_totales/cantidad;
+	else set media = 0;
+	end if;
+	
+	update contenido set puntuacion_media = media;
+end$$
+delimiter ;
+#----------------------------------------------------------------------
+#-- TRIGGER que llama a ACTUALIZAR_NOTA al hacer INSERT en VALORACION
+#----------------------------------------------------------------------
+delimiter $$
+drop trigger if exists trigger_actualizar_nota$$
+create trigger trigger_actualizar_nota
+after insert on valoracion for each row
+begin
+	call actualizar_nota(new.contenido_id);
+end$$
+delimiter ;
+
+#-----------------------------------------------------------------------------------------
+#-- TABLA CARRITO 
+#-----------------------------------------------------------------------------------------
+#-- PROCEDIMIENTO que actualiza el precio del carrito activo según las líneas de factura
+#-- le pasamos el cliente_id para identificar el carrito activo
+#-----------------------------------------------------------------------------------------
 delimiter $$
 drop procedure if exists actualizar_precio_carrito$$
 create procedure actualizar_precio_carrito(in p_cliente_id int)
@@ -137,38 +198,68 @@ begin
 
 	update carrito set total = precio_total where id = carrito_activo_id;
 
-end
-$$
+end$$
 delimiter;
 
--- #### TABLA LIN_FAC ####
--- PROCEDIMIENTO al que llamará el botón de añadir al carrito en la vista detallada del contenido
--- le pasamos el cliente_id porque buscará luego el carrito activo del cliente
+#-----------------------------------------------------------------------------------------
+#-- TABLAS DE CONTENIDO
+#-----------------------------------------------------------------------------------------
+#-- TRIGGER para definir el precio en PELICULA
+#-----------------------------------------------------------------------------------------
 delimiter $$
-drop procedure if exists anyadir_al_carrito$$
-create procedure anyadir_al_carrito(in p_cliente_id int, in p_contenido_id int)
+drop trigger if exists trigger_pelicula_precio$$
+create trigger trigger_pelicula_precio
+before insert on pelicula
+for each row
 begin
-	declare carrito_activo_id int;
-	declare contenido_precio decimal(10,2);
-	-- variable que recoge el id del carrito activo del cliente
-	select id into carrito_activo_id from carrito
-	where cliente_id = p_cliente_id and activo = true;
-	-- variable que recoge el precio según el tipo de contenido
-	if p_contenido_id in (select contenido_id from pelicula)
-		then set contenido_precio = (select precio from pelicula where contenido_id = p_contenido_id);
-	 elseif p_contenido_id in (select contenido_id from serie)
-	 	then set contenido_precio = (select precio from serie where contenido_id = p_contenido_id);
-	 elseif p_contenido_id in (select contenido_id from corto)
-	 	then set contenido_precio = (select precio from corto where contenido_id = p_contenido_id);
-	else set contenido_precio = 0.00;
-	end if; 	
-	insert into lin_fac(carrito_id, contenido_id, precio) 
-	values (carrito_activo_id, p_contenido_id, contenido_precio);
+    declare porcentaje decimal(5,4);
 
-	call actualizar_precio_carrito(p_cliente_id);
-end
-$$
+    select t.porcentaje into porcentaje
+    from contenido c
+    join tarifa t on c.tarifa_id = t.id
+    where c.id = new.contenido_id;
+
+    set new.precio = new.precio_base + (new.precio_base * porcentaje);
+end$$
 delimiter ;
+#-----------------------------------------------------------------------------------------
+#-- PROCEDIMIENTO para actualizar el precio en SERIE al insertar en CAPITULO
+#-- se llamará desde un TRIGGER INSERT en CAPITULO
+#-----------------------------------------------------------------------------------------
+delimiter $$
+drop procedure if exists actualizar_precio_serie$$
+create procedure actualizar_precio_serie(in p_serie_id int)
+begin
+    declare precio_total decimal(10,2);
 
+    select sum(precio) into precio_total
+    from capitulo c
+    join temporada t on c.temporada_id = t.id
+    where t.serie_id = p_serie_id;
+
+    if precio_total is null then
+        set precio_total = 0.00;
+    end if;
+
+    update serie set precio = precio_total where contenido_id = p_serie_id;
+end$$
+delimiter ;
+#-----------------------------------------------------------------------------------------
+#-- TRIGGER que actualiza el precio de SERIE al insertar en CAPITULO
+#-----------------------------------------------------------------------------------------
+
+delimiter $$
+drop trigger if exists trigger_actualizar_precio_serie_after_insert_capitulo$$
+create trigger trigger_actualizar_precio_serie_after_insert_capitulo
+after insert on capitulo
+for each row
+begin
+    declare serie_id int;
+
+    select serie_id into serie_id from temporada where id = new.temporada_id;
+
+    call actualizar_precio_serie(serie_id);
+end$$
+delimiter ;
 
 
