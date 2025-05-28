617c22f942b3b3dbe7ff4d345611d0d5f33d120a
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-05-28 11:54:05 +0200

    cambiado la direccion ip

--- app/src/main/java/es/ieslavereda/miravereda/Base/Parameters.java
+++ app/src/main/java/es/ieslavereda/miravereda/Base/Parameters.java
@@ -2,7 +2,7 @@ package es.ieslavereda.miravereda.Base;
 
 public class Parameters {
 
-    public static final String URL_API_BASE = "http://172.28.201.195:8080/";
+    public static final String URL_API_BASE = "http://172.28.201.195:3306/";
     public static final String URL_IMAGE_BASE = "https://my-web.joaalsai.com/images/";
 }
 
