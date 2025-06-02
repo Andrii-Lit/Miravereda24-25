1fae175cd001e945e02df07a4b03d03109392b82
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-06-02 15:55:30 +0200

    script contenido corregido

--- SCRIPT_CONTENIDO_miravereda2425.sql
+++ SCRIPT_CONTENIDO_miravereda2425.sql
@@ -1,5 +1,5 @@
 -- Inserción de una serie
-insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path, tipo, precio)
+insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
 values
 (1001, 
  'Breaking Bad', 
@@ -10,17 +10,15 @@ values
  'Bryan Cranston, Aaron Paul', 
  '2008-09-15', 
  8.7, 
- 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpicfiles.alphacoders.com%2F422%2Fthumb-1920-422251.jpg&f=1&nofb=1&ipt=8fb2389bbe6fbacb1da3511e95cc76d698d836cf8499113bb2d5b1e16f2cf662', 
- 'serie', 
- 10.00
+ 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpicfiles.alphacoders.com%2F422%2Fthumb-1920-422251.jpg&f=1&nofb=1&ipt=8fb2389bbe6fbacb1da3511e95cc76d698d836cf8499113bb2d5b1e16f2cf662'
 );
 
-insert into serie(contenido_id, tarifa_id, disponibilidad, precio, precio_base) 
+insert into serie(contenido_id, tarifa_id, disponibilidad, precio_base) 
 values 
-(1001, 2, true, 10.00, 10.00);
+(1001, 2, true, 10.00);
 
 -- Inserción de un corto
-insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path, tipo, precio)
+insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
 values
 (2003,
  'Paperman',
@@ -31,24 +29,22 @@ values
  'Shane Prigmore, Kari Wahlgren',
  '2012-11-02',
  8.2,
- 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi2.wp.com%2Fwww.heyuguys.com%2Fimages%2F2012%2F07%2FPaperman-poster.jpg&f=1&nofb=1&ipt=272ca1f030c5f227c3357317c56ad4601b8f86d29af6cc41281db6b909a9490f',
- 'corto',
- 4.00
+ 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi2.wp.com%2Fwww.heyuguys.com%2Fimages%2F2012%2F07%2FPaperman-poster.jpg&f=1&nofb=1&ipt=272ca1f030c5f227c3357317c56ad4601b8f86d29af6cc41281db6b909a9490f'
 );
 
-insert into corto(contenido_id, tarifa_id, disponibilidad, precio, precio_base)
+insert into corto(contenido_id, tarifa_id, precio_base)
 values
-(2003, 3, true, 4.00, 4.00);
+(2003, 3, 4.00);
 
 
 -- Inserción de varias películas
-insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path, tipo, precio)
+insert into contenido(id, titulo, descripcion, genero, nombre_dir, duracion, actores_principales, fecha_estreno, puntuacion_media, poster_path)
 values
-(3001, 'Cadena Perpetua', 'The Shawshank Redemption es una película dramática escrita y dirigida por el estadounidense Frank Darabont, y producida por los estudios Castle Rock Entertainment, cuyo estreno se produjo en 1994. Su trama está basada en Rita Hayworth y la redención de Shawshank, una novela corta de Stephen King, y abarca una mirada optimista de la vida relatando la historia del banquero Andy, quien es condenado a cadena perpetua en la Penitenciaría Estatal de Shawshank, a pesar de haberse declarado inocente.', 'Drama', 'Miguel Ángel', 110, 'Elena Ríos, David Martín', '2023-05-05', 8.1, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.LpafPZQdQKzgnkBaHBbOJwHaJ6%26pid%3DApi&f=1&ipt=17b9db332fe62c58fe907ed9b5ca2a4e2f59781f89fa111f350d4ff234f97b1e&ipo=images', 'pelicula', 6.00),
-(3002, 'Shutter Island', 'En el verano de 1954, los agentes judiciales Teddy Daniels (DiCaprio) y Chuck Aule (Ruffalo) son destinados a una remota isla del puerto de Boston para investigar la desaparición de una peligrosa asesina (Mortimer) que estaba recluida en el hospital psiquiátrico Ashecliffe, un centro penitenciario para criminales perturbados dirigido por el siniestro doctor John Cawley (Kingsley). Pronto descubrirán que el centro guarda muchos secretos y que la isla esconde algo más peligroso que los pacientes. Thriller psicológico basado en la novela homónima de Dennis Lehane (autor de "Mystic River" y "Gone Baby Gone"). (FILMAFFINITY)', 'Thriller', 'Sonia López', 105, 'Carlos Ruiz, Marta Díaz', '2023-08-01', 7.2, 'https://pics.filmaffinity.com/shutter_island-982808260-large.jpg', 'pelicula', 5.50),
-(3003, 'El viaje de Chihiro', 'Chihiro es una niña de diez años que viaja en coche con sus padres. Después de atravesar un túnel, llegan a un mundo fantástico, en el que no hay lugar para los seres humanos, sólo para los dioses de primera y segunda clase. Cuando descubre que sus padres han sido convertidos en cerdos, Chihiro se siente muy sola y asustada. (FILMAFFINITY)', 'Animación', 'Pablo Gómez', 90, 'Voces: Ana Martínez, Luis Gómez', '2023-09-01', 7.9, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fdoblaje%2Fimages%2Ff%2Ffc%2FEL_VIAJE_DE_CHIHIRO.jpg%2Frevision%2Flatest%3Fcb%3D20170506234618%26path-prefix%3Des&f=1&nofb=1&ipt=35584f7d9b75353a9be8a57427215252259a31cb90e1669b0828ef0b922668c5', 'pelicula', 5.00);
+(3001, 'Cadena Perpetua', 'The Shawshank Redemption es una película dramática escrita y dirigida por el estadounidense Frank Darabont, y producida por los estudios Castle Rock Entertainment, cuyo estreno se produjo en 1994. Su trama está basada en Rita Hayworth y la redención de Shawshank, una novela corta de Stephen King, y abarca una mirada optimista de la vida relatando la historia del banquero Andy, quien es condenado a cadena perpetua en la Penitenciaría Estatal de Shawshank, a pesar de haberse declarado inocente.', 'Drama', 'Miguel Ángel', 110, 'Elena Ríos, David Martín', '2023-05-05', 8.1, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.LpafPZQdQKzgnkBaHBbOJwHaJ6%26pid%3DApi&f=1&ipt=17b9db332fe62c58fe907ed9b5ca2a4e2f59781f89fa111f350d4ff234f97b1e&ipo=images'),
+(3002, 'Shutter Island', 'En el verano de 1954, los agentes judiciales Teddy Daniels (DiCaprio) y Chuck Aule (Ruffalo) son destinados a una remota isla del puerto de Boston para investigar la desaparición de una peligrosa asesina (Mortimer) que estaba recluida en el hospital psiquiátrico Ashecliffe, un centro penitenciario para criminales perturbados dirigido por el siniestro doctor John Cawley (Kingsley). Pronto descubrirán que el centro guarda muchos secretos y que la isla esconde algo más peligroso que los pacientes. Thriller psicológico basado en la novela homónima de Dennis Lehane (autor de "Mystic River" y "Gone Baby Gone"). (FILMAFFINITY)', 'Thriller', 'Sonia López', 105, 'Carlos Ruiz, Marta Díaz', '2023-08-01', 7.2, 'https://pics.filmaffinity.com/shutter_island-982808260-large.jpg'),
+(3003, 'El viaje de Chihiro', 'Chihiro es una niña de diez años que viaja en coche con sus padres. Después de atravesar un túnel, llegan a un mundo fantástico, en el que no hay lugar para los seres humanos, sólo para los dioses de primera y segunda clase. Cuando descubre que sus padres han sido convertidos en cerdos, Chihiro se siente muy sola y asustada. (FILMAFFINITY)', 'Animación', 'Pablo Gómez', 90, 'Voces: Ana Martínez, Luis Gómez', '2023-09-01', 7.9, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fdoblaje%2Fimages%2Ff%2Ffc%2FEL_VIAJE_DE_CHIHIRO.jpg%2Frevision%2Flatest%3Fcb%3D20170506234618%26path-prefix%3Des&f=1&nofb=1&ipt=35584f7d9b75353a9be8a57427215252259a31cb90e1669b0828ef0b922668c5');
 
-insert into pelicula(contenido_id, tarifa_id, disponible_hasta, precio, precio_base) values
-(3001, 1, '2025-12-31', 5.00, 5.00),
-(3002, 1, '2025-12-31', 4.50, 4.50),
-(3003, 1, '2025-12-31', 6.00, 6.00);
+insert into pelicula(contenido_id, tarifa_id, disponible_hasta, precio_base) values
+(3001, 1, '2025-12-31', 5.00),
+(3002, 1, '2025-12-31', 4.50),
+(3003, 1, '2025-12-31', 6.00);
