091a4040d3d45ce08355f78b6f190bc96bc9d760
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-06-02 09:21:45 +0200

    añadido ip al subdomain

--- app/src/main/java/es/ieslavereda/miravereda/Base/Parameters.java
+++ app/src/main/java/es/ieslavereda/miravereda/Base/Parameters.java
@@ -2,7 +2,7 @@ package es.ieslavereda.miravereda.Base;
 
 public class Parameters {
 
-    public static final String URL_API_BASE = "http://10.0.2.2:8080/api/"; //Sustituir ip
+    public static final String URL_API_BASE = "http://10.249.159.10:8080/api/"; //Sustituir ip
     public static final String URL_IMAGE_BASE = "https://my-web.joaalsai.com/images/";
 }
 
