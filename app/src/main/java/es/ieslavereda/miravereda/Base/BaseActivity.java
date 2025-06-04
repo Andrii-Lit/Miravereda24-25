d0b68c4a8440ad82bfa0a43853121ff87557fb9f
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-06-04 09:54:34 +0200

    ahora la barra de navegación no aparece y el carrito tiene un icono de carrito

--- app/src/main/java/es/ieslavereda/miravereda/Base/BaseActivity.java
+++ app/src/main/java/es/ieslavereda/miravereda/Base/BaseActivity.java
@@ -4,6 +4,7 @@ package es.ieslavereda.miravereda.Base;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
@@ -27,6 +28,14 @@ public  class BaseActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             connector = Connector.getConector();
             progressBar = new MyProgressBar(this);
+            hideSystemUI();
+        }
+        @Override
+        public void onWindowFocusChanged(boolean hasFocus) {
+            super.onWindowFocusChanged(hasFocus);
+            if (hasFocus) {
+                hideSystemUI();
+            }
         }
 
         protected<T> void executeCall(CallInterface<T>callInterface){
@@ -68,6 +77,16 @@ public  class BaseActivity extends AppCompatActivity {
             progressBar.initControl(rootView);
             hideProgress();
         }
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
 
 
 }
