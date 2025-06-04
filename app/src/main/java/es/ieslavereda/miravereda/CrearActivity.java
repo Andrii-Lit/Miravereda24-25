9465f6e6f3b344412959ba07523c5e7df85925e7
cristobalMadariaga <crimadmor@alu.edu.gva.es>
2025-06-04 08:59:08 +0200

    crear completamente funcional, falta solo diseño de la aplicación

--- app/src/main/java/es/ieslavereda/miravereda/CrearActivity.java
+++ app/src/main/java/es/ieslavereda/miravereda/CrearActivity.java
@@ -1,17 +1,21 @@
 package es.ieslavereda.miravereda;
 
+import static android.widget.Toast.LENGTH_LONG;
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 
-import java.time.LocalDate;
-import java.util.Date;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.time.LocalDate;
 import es.ieslavereda.miravereda.API.Connector;
 import es.ieslavereda.miravereda.Base.BaseActivity;
 import es.ieslavereda.miravereda.Base.CallInterface;
@@ -21,18 +25,19 @@ public class CrearActivity extends BaseActivity {
     private Cliente cliente = null;
     private EditText crear_nombreET, crear_apellidosET, crear_fecha_nacET,
             crear_cod_postalET,crear_direccionET, crear_num_tarjetaET, crear_emailET, crear_contrasenyaET;
-
-    private Button crear_backBTN, crear_BTN;
+    private FloatingActionButton crear_backBTN;
+    private Button crear_BTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences preferences = getSharedPreferences("config", MODE_PRIVATE);
         int tema = preferences.getInt("tema", 0);
         setTheme(tema);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear);
         EdgeToEdge.enable(this);
 
-        crear_nombreET = findViewById(R.id.crear_apellidosET);
-        crear_apellidosET = findViewById(R.id.crear_nombreET);
+        crear_nombreET = findViewById(R.id.crear_nombreET);
+        crear_apellidosET = findViewById(R.id.crear_apellidosET);
         crear_fecha_nacET = findViewById(R.id.crear_fecha_nacET);
         crear_cod_postalET = findViewById(R.id.crear_cod_postalET);
         crear_direccionET = findViewById(R.id.crear_direccionET);
@@ -46,6 +51,7 @@ public class CrearActivity extends BaseActivity {
         crear_backBTN.setOnClickListener((View view)->{
             Intent intent = new Intent();
             setResult(RESULT_CANCELED, intent);
+            Toast.makeText(CrearActivity.this, "Acción cancelada", LENGTH_SHORT).show();
             finish();
         });
 
@@ -72,6 +78,7 @@ public class CrearActivity extends BaseActivity {
             public void doInUI(Cliente data) {
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
+                Toast.makeText(CrearActivity.this, "Cuenta creada con éxito", LENGTH_LONG).show();
                 finish();
             }
         });
